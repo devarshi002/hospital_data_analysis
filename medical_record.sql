56a1ee7910f51a2b38e78099f847a40a0c21faab
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-22 18:22:17 +0530

    updated medical record table

--- medical_record.sql
+++ medical_record.sql
@@ -15,3 +15,24 @@ FROM medicalrecords
 WHERE Diagnosis = "Heart attack";
 
 -- Find the most recent medical record for each patient
+
+--  Find the most recent visit date for each patient
+SELECT Patient_ID, max(Visit_Date) as recent_visit
+FROM medicalrecords
+GROUP BY Patient_ID
+ORDER BY recent_visit DESC;
+
+
+-- Retrieve all records of patients treated by a specific doctor (e.g., Doctor_ID = 3)
+SELECT * FROM medicalrecords WHERE Doctor_ID = 3;
+
+-- Count the number of records for each diagnosis
+SELECT Diagnosis, count(*) as no_of_records
+FROM medicalrecords
+GROUP BY Diagnosis;
+
+--List all prescriptions given for a specific treatment (e.g., 'Physical Therapy')
+
+SELECT * FROM medicalrecords
+WHERE Treatment like "%Physical%";
+
--- mysql local.session.sql
+++ mysql local.session.sql
@@ -1,1 +1,1 @@
-SELECT * FROM patients;
+SELECT * FROM appointments;
